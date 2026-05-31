faf34fcd35d453fcae9916b41d789b9cc459b2e7
Jan Olaf Martin <jan.krems@gmail.com>
2026-05-30 22:29:54 -0700

    Use real wasm js-string builtins + importedStringConstants (#93)
    * Use wasm js-string builtins and importedStringConstants
    
    * Align js-string import signatures with externref
    
    * Use required concat builtin signature

--- crates/waluau-codegen-wasm/src/host.rs
+++ crates/waluau-codegen-wasm/src/host.rs
@@ -5,12 +5,13 @@ use std::collections::HashMap;
 use waluau_diagnostics::Diagnostic;
 use waluau_ir::{Function as IrFunction, Instruction as IrInstruction, Module};
 
+pub const IMPORTED_STRING_CONSTANTS_MODULE: &str = "string_constants";
+pub const JS_STRING_BUILTINS_MODULE: &str = "wasm:js-string";
 pub const IMPORT_MODULE: &str = "waluau";
 pub const CUSTOM_SECTION_NAME: &str = "waluau.strc";
 
-pub const IMPORT_JS_STRING_CONST: &str = "js_string_const";
-pub const IMPORT_JS_STRING_EQ: &str = "js_string_eq";
-pub const IMPORT_JS_STRING_CONCAT: &str = "js_string_concat";
+pub const IMPORT_JS_STRING_EQ: &str = "equals";
+pub const IMPORT_JS_STRING_CONCAT: &str = "concat";
 pub const IMPORT_JS_TOSTRING_I32: &str = "js_tostring_i32";
 pub const IMPORT_JS_TOSTRING_U32: &str = "js_tostring_u32";
 pub const IMPORT_JS_TOSTRING_I64: &str = "js_tostring_i64";
@@ -21,31 +22,29 @@ pub const IMPORT_JS_TOSTRING_BOOL: &str = "js_tostring_bool";
 pub const IMPORT_PRINT: &str = "print";
 
 /// Number of imported host functions emitted before user-defined functions.
-pub const HOST_IMPORT_COUNT: u32 = 11;
+pub const HOST_IMPORT_COUNT: u32 = 10;
 
 /// Function index of the first user-defined function in the combined import+defined index space.
 pub const fn defined_func_index(user_index: u32) -> u32 {
     HOST_IMPORT_COUNT + user_index
 }
 
-/// Index of `IMPORT_JS_STRING_CONST` in the combined function index space.
-pub const IMPORT_JS_STRING_CONST_FUNC: u32 = 0;
 /// Index of `IMPORT_JS_STRING_EQ` in the combined function index space.
-pub const IMPORT_JS_STRING_EQ_FUNC: u32 = 1;
+pub const IMPORT_JS_STRING_EQ_FUNC: u32 = 0;
 /// Index of `IMPORT_JS_STRING_CONCAT` in the combined function index space.
-pub const IMPORT_JS_STRING_CONCAT_FUNC: u32 = 2;
+pub const IMPORT_JS_STRING_CONCAT_FUNC: u32 = 1;
 /// Index of `IMPORT_PRINT` in the combined function index space.
-pub const IMPORT_PRINT_FUNC: u32 = 3;
-pub const IMPORT_JS_TOSTRING_I32_FUNC: u32 = 4;
-pub const IMPORT_JS_TOSTRING_U32_FUNC: u32 = 5;
-pub const IMPORT_JS_TOSTRING_I64_FUNC: u32 = 6;
-pub const IMPORT_JS_TOSTRING_U64_FUNC: u32 = 7;
-pub const IMPORT_JS_TOSTRING_F32_FUNC: u32 = 8;
-pub const IMPORT_JS_TOSTRING_F64_FUNC: u32 = 9;
-pub const IMPORT_JS_TOSTRING_BOOL_FUNC: u32 = 10;
+pub const IMPORT_PRINT_FUNC: u32 = 2;
+pub const IMPORT_JS_TOSTRING_I32_FUNC: u32 = 3;
+pub const IMPORT_JS_TOSTRING_U32_FUNC: u32 = 4;
+pub const IMPORT_JS_TOSTRING_I64_FUNC: u32 = 5;
+pub const IMPORT_JS_TOSTRING_U64_FUNC: u32 = 6;
+pub const IMPORT_JS_TOSTRING_F32_FUNC: u32 = 7;
+pub const IMPORT_JS_TOSTRING_F64_FUNC: u32 = 8;
+pub const IMPORT_JS_TOSTRING_BOOL_FUNC: u32 = 9;
 
 /// Number of host function types emitted after array types in the type section.
-pub const HOST_TYPE_COUNT: u32 = 8;
+pub const HOST_TYPE_COUNT: u32 = 7;
 
 pub fn encode_string_constants_section(strings: &[String]) -> Vec<u8> {
     let mut out = Vec::new();
